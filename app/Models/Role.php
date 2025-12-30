5ac6ca9bc8d00e26e23bcb4f07de66aadeceab6b
trongnd106 <trong.nd106@gmail.com>
2025-12-31 00:31:26 +0700

    add return get guard name

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,4 +6,14 @@ class Role extends BaseModel
     protected $fillable = [
         'name',
     ];
+
+    /**
+     * Get the guard_name attribute (always return 'api' for User roles)
+     * This is needed because Spatie Permission package requires guard_name
+     * but we removed it from the database table
+     */
+    public function getGuardNameAttribute(): string
+    {
+        return 'api';
+    }
 }
